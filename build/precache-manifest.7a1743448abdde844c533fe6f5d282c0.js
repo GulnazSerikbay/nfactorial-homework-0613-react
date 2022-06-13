927af13694dfe17ecfb1741ca1b7c2153b90c0d8
Gulnaz Serikbay <gserikba@andrew.cmu.edu>
2022-06-13 19:21:41 +0600

    build

---
+++ build/precache-manifest.7a1743448abdde844c533fe6f5d282c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c29572fcacbd7f8fa9a",
+    "url": "/static/css/main.337f54a0.chunk.css"
+  },
+  {
+    "revision": "6c29572fcacbd7f8fa9a",
+    "url": "/static/js/main.a0b080b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0deb7e6cb8ff71a9d4a8",
+    "url": "/static/css/2.5e104779.chunk.css"
+  },
+  {
+    "revision": "0deb7e6cb8ff71a9d4a8",
+    "url": "/static/js/2.f0b2472d.chunk.js"
+  },
+  {
+    "revision": "9248aeed94229dc775703843ab5ec384",
+    "url": "/static/media/1.9248aeed.jpeg"
+  },
+  {
+    "revision": "6644a44a32a970a9fd7bee57b297e3c0",
+    "url": "/static/media/2.6644a44a.jpeg"
+  },
+  {
+    "revision": "23ad840326190187c5227dca8a0ae283",
+    "url": "/static/media/3.23ad8403.jpeg"
+  },
+  {
+    "revision": "f6f654e963cc7c1697cd0ab9298050a5",
+    "url": "/static/media/4.f6f654e9.jpeg"
+  },
+  {
+    "revision": "2d5ae3d98da572a1ea3b6b48c16a8515",
+    "url": "/static/media/5.2d5ae3d9.jpeg"
+  },
+  {
+    "revision": "5ac279e5f3c0202d59f4c9b72603aad9",
+    "url": "/index.html"
+  }
+];
